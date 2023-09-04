f8e12b5ee5b8635b3151068c0426abd89953021a
Sal Rahman <salehen.rahman@gmail.com>
2023-09-03 21:15:56 -0700

    Some more work

--- httphelpers/method.go
+++ httphelpers/method.go
@@ -1,7 +1,10 @@
 package httphelpers
 
+import "fmt"
+
 func Method(method string) Processor {
 	return Condition(func(r BarebonesRequest) bool {
+		fmt.Println("Method", r.Method, method)
 		return r.Method == method
 	})
 }
